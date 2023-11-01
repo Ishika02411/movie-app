9ac313d467312c66d4a12302b6b60472e2b6c76e
Ishika02411 <chauhanishika@gmail.com>
2023-11-02 01:22:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-
+// import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      hello world
+      <Router>
+        <Routes>
+          <Route index element= {<h1>Hello world</h1>}> </Route>
+           <Route path= "movie/:id" element={<h1> movie detai page</h1>}> </Route>
+          <Route path= "movie/:type" element={<h1> movie list page</h1>}> </Route>
+          <Route path= "/*" element={<h1> error page</h1>}> </Route> 
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-)
-
+);
